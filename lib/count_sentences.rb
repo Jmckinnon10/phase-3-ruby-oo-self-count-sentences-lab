edf9f133d86da0f1fe95bbe8837d8a4412c4f76e
John McKinnon <johnmckinnon23@yahoo.com>
2022-11-23 14:50:58 -0700

    commit tree 716d554fc621024adeb04f92453e0d19eb56a1e6

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+     self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?('!') ? true : false
   end
 
   def count_sentences
+   if self.count_sentences == 0
+    !self.count_sentences
+   else
+    self.count_sentences
 
+
+    binding.pry
+   end
   end
 end
